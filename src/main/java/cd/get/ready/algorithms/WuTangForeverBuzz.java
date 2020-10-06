39ddfd2e70bc176bd9a69fbd627ca2873724f5a1
Habib's World <62362787+HabibHassan@users.noreply.github.com>
2020-10-06 02:10:15 -0400

    Add files via upload
    Problem 1 My Answers

--- src/main/java/cd/get/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/cd/get/ready/algorithms/WuTangForeverBuzz.java
@@ -3,6 +3,22 @@ package cd.get.ready.algorithms;
 public class WuTangForeverBuzz {
 
     public String wuTangClan(int value){
-        return null;
+        String number = String.valueOf(value)
+        for(int i=0; i<value; i++)
+            if(value%3 == 0){
+                return "Wu";
+            }
+        else if (value%3 == 0 && value%5 ==0){
+            return "WuTangForever";
+
+        }
+        else if (value%5 == 0){
+            return "Tang";
+        }
+        return value;
     }
+
 }
+
+
+
